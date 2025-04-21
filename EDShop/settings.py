1d09fb2135f046998d050fb86074a6baf51d41d9
jpca92 <jpca92@gmail.com>
2025-04-21 14:44:26 -0500

    lesson 1_5

--- EDShop/settings.py
+++ EDShop/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -111,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Bogota'
 
 USE_I18N = True
 
@@ -127,3 +127,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- appweb/models.py
+++ appweb/models.py
@@ -1,3 +1,24 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    registry_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.RESTRICT)
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    registry_date = models.DateTimeField(default=timezone.now)
+    image = models.ImageField(upload_to='products', blank=True, null=True)
+    stock = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
