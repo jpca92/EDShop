9e084699b9d19dde495be2ea2f8d48a52cdead53
jpca92 <jpca92@gmail.com>
2025-04-21 15:10:38 -0500

    lesson 1_5

--- appweb/admin.py
+++ appweb/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Product
+
+admin.site.register(Category)
+# admin.site.register(Product)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'registry_date')
+    list_editable = ('price',)
